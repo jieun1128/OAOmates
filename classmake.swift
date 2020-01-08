3d3fbad84d91a7d74d544cf926b7fdf9bb7afe19
kyeongseo90 <y0524y@naver.com>
2020-01-08 17:52:57 +0900

    Update classmake.swift

--- classmake.swift
+++ classmake.swift
@@ -6,13 +6,17 @@ class Person { // 개인이 회원가입 했을 때 개인에게 생성(배정)�
     let name
     var nickname
     enum age{
-        case 19, 20, 21, 22, 23, 24, 25} //등
+        case 19, 20, 21, 22, 23, 24, 25
+    } //등
     enum grade{
-        case 1, 2, 3, 4, 5}
+        case 1, 2, 3, 4, 5
+    }
     enum nationality{
-        case korea, japan, china, russia, india}
+        case korea, japan, china, russia, india
+    }
     enum major{
-        case 불교대학, 문과대학, 이과대학, 법과대학, 사회과학대학, 경찰사법대학, 경영대학, 바이오시스템대학, 공과대학, 사범대학, 예술대학, 약학대학, 미래융합대학, 연계전공}
+        case 불교대학, 문과대학, 이과대학, 법과대학, 사회과학대학, 경찰사법대학, 경영대학, 바이오시스템대학, 공과대학, 사범대학, 예술대학, 약학대학, 미래융합대학, 연계전공
+    }
     
 }
 
@@ -24,7 +28,7 @@ class Dorm { // 기숙사 생성. 기숙사를 구분하는 정보. 다음 4기�
         case man, woman
     }
     enum dormitory{
-        
+        case 남산학사, 충무학사, 고양학사
     }
     
 }
