3ca05475fe3429a5be4f4b9e29f818328303fce0
jieunPark <5656jieun@naver.com>
2020-01-20 16:32:33 +0900

    홈뷰 폴더
    이번엔 제발 별 탈없이 만들어졌으면 좋겠어요,,

---
+++ roommate/OAOmates/HomeView/Room.swift
@@ -0,0 +1,16 @@
+//
+//  Room.swift
+//  OAOmates
+//
+//  Created by linc on 2020/01/20.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import Foundation
+struct Room {
+    var dormitory: String
+    var number: Int
+    var age: Int
+    var gender: String
+    var nation: String
+}
