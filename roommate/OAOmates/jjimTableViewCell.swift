ee2ef0447857a762305807cecb8df5e74ca23bc0
kyeongseo90 <y0524y@naver.com>
2020-01-20 18:40:50 +0900

    1/20

---
+++ roommate/OAOmates/jjimTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  jjimView.swift
+//  OAOmates
+//
+//  Created by linc on 2020/01/20.
+//  Copyright © 2020 linc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class jjimTableViewCell: UITableViewCell {
+    
+}
