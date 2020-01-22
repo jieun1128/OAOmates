9045d85ec79feebf1b61bb35edecce016c0c05d1
kyeongseo90 <y0524y@naver.com>
2020-01-22 19:17:59 +0900

    1/22최종

--- roommate/OAOmates/jjimView/jjimTableViewCell.swift
+++ roommate/OAOmates/jjimView/jjimTableViewCell.swift
@@ -10,11 +10,9 @@ import Foundation
 import UIKit
 
 class jjimTableViewCell: UITableViewCell {
-    @IBOutlet weak var dormLabel: UILabel!
+
+    @IBOutlet weak var dormitoryLabel: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
-    @IBOutlet weak var nationLabel: UILabel!
-    @IBOutlet weak var progressView: UIProgressView!
-    
 }
