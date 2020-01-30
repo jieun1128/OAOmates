41964713da9b66f116b81b90a3a41a2a238c61eb
연진 <ygeenee@naver.com>
2020-01-30 16:44:09 +0900

    찜 뷰 삭제하고 마이뷰 데이터넣는걸 시도

--- roommate/OAOmates/MyView/MyViewController.swift
+++ roommate/OAOmates/MyView/MyViewController.swift
@@ -9,21 +9,42 @@
 import UIKit
 
 
-class MyViewController: UITableViewController {
 
+
+class MyViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBAction func unwindVC1 (segue : UIStoryboardSegue){}
+    
+    @IBOutlet var myTableView: UITableView!
 
     fileprivate var  data:[Room] = [] {
                didSet {
-                   
+                self.myTableView.reloadData()
                }
            }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.myTableView.dataSource = self
+        self.myTableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
-    
+    override func viewWillAppear (_ animated: Bool){
+//        API.shared.allUsers{(users) in
+//            self.data = users
+//        }
+    }
+}
+
+extension MyViewController: UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return self.data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableView{
+
+}
+
 
     /*
     // MARK: - Navigation
@@ -35,4 +56,4 @@ class MyViewController: UITableViewController {
     }
     */
 
-}
+
